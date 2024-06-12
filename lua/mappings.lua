fab45929ad26d583e1ab136a173482ff3e0d9bb4
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-06-12 23:00:36 +0800

    feat: remove some useless keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,10 @@
 require "nvchad.mappings"
 
+-- Set mappings
 local map = vim.keymap.set
+
+-- Disable mappings
+local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -13,6 +17,19 @@ end, { desc = "goto next buffer" })
 map("n", "<S-H>", function() -- S-TAB
   require("nvchad.tabufline").prev()
 end, { desc = "goto prev buffer" })
+
+-- remap the window switch
+map("n", "<Tab>", "<C-w>w", { desc = "switch to next window" })
+
+-- toggle terminal
+map("n", "<leader>t", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "toggle terminal" })
+
+-- unmap some useless keys
+nomap({ "n", "t" }, "<A-v>")
+nomap({ "n", "t" }, "<A-h>")
+nomap({ "n", "t" }, "<A-i>")
 
 -- open file search
 map("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
