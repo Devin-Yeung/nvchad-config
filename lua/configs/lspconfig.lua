338ed41248983e42f41a5ad54d50581589a93657
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-06-12 17:50:52 +0800

    refactor: refactor lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,13 @@
 -- lsp config
-local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_attach = function(client, bufnr)
+  require("nvchad.configs.lspconfig").on_attach(client, bufnr)
+  -- neovim support inlay_hint natively after neovim 0.10
+  -- Note: that the hints are only visible after rust-analyzer has finished loading and you have to edit the file to trigger a re-render.
+  vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+  -- for those who use (0.8 < neovim < 0.10)
+  -- require("lsp-inlayhints").on_attach(client, bufnr)
+end
+
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
@@ -9,14 +17,7 @@ local servers = { "html", "cssls", "rust_analyzer" }
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = function(client, bufnr)
-      on_attach(client, bufnr)
-      -- neovim support inlay_hint natively after neovim 0.10
-      -- Note: that the hints are only visible after rust-analyzer has finished loading and you have to edit the file to trigger a re-render.
-      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-      -- for those who use (0.8 < neovim < 0.10)
-      -- require("lsp-inlayhints").on_attach(client, bufnr)
-    end,
+    on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
   }
