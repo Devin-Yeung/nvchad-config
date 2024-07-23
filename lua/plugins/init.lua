8284ffa4a8de5739fb1bf34389fbe8b0f863e02a
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-07-23 22:50:14 +0800

    feat: add markdown.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -58,7 +58,7 @@ return {
       -- disable default mappings
       vim.g.VM_default_mappings = 0
       vim.g.VM_maps = {
-        ["Find Under"] = "<C-n>"
+        ["Find Under"] = "<C-n>",
       }
     end,
   },
@@ -66,6 +66,16 @@ return {
   {
     "github/copilot.vim",
     lazy = false,
+  },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    lazy = false,
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   },
   -- Inlay hints for those (0.8 <= neovim < 0.10)
   -- {
